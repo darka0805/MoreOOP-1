6202680ae65bb3fb257a06c517f903f7b1f141bc
darka0805 <kuzyshyn.pn@ucu.edu.ua>
2023-10-09 17:25:11 +0300

    new

--- src/main/java/lotr/King.java
+++ src/main/java/lotr/King.java
@@ -1,9 +1,10 @@
 package lotr;
-import kick.KnightStrategy;
-import java.util.Random;
 
-public class King extends Character{
+import java.util.Random;
+import kick.KingStrategy;
+
+public class King extends Character {
     public King() {
-        super(new Random().nextInt(15 - 5 + 1) + 2, new Random().nextInt(15 - 5 + 1) + 2,  new KnightStrategy());
+        super(new Random().nextInt(11) + 5, new Random().nextInt(11) + 5,  new KingStrategy());
     }
 }
